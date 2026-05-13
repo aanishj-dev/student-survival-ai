8edd233549fe8ba5b3ba3c92d347cf3d8a0c742b
Musadiq Mateen <musadiqmateen@gmail.com>
2026-05-14 00:36:21 +0100

    Moved firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgl1dPSYJvbjguHjlkCbJvfVFt8GgjdCs",
-  authDomain: "student-survival-ai.firebaseapp.com",
-  projectId: "student-survival-ai",
-  storageBucket: "student-survival-ai.firebasestorage.app",
-  messagingSenderId: "103759488492",
-  appId: "1:103759488492:web:ea94cf0612c3087e784cc9",
-  measurementId: "G-H17THZ88NX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
